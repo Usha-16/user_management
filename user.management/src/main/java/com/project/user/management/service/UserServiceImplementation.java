e41676fc1ec9f567adf778a05e4ab48b44d08f4e
Usha-16 <usha.pattanashetty@gmail.com>
2026-05-06 19:16:30 +0530

    project was set up

---
+++ user.management/src/main/java/com/project/user/management/service/UserServiceImplementation.java
@@ -0,0 +1,17 @@
+package com.project.user.management.service;
+
+import com.project.user.management.repository.UsersRepository;
+
+public class UserServiceImplementation 
+               implements UsersService{
+	
+	UsersRepository repo;
+
+	public UserServiceImplementation(UsersRepository repo) {
+		super();
+		this.repo = repo;
+	}
+	
+	
+
+}
